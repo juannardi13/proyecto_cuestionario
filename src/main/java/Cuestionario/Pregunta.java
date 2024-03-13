e429ea6723f409b8fe80b9029a0992bb112470fa
juannardi13 <jnardi@frba.utn.edu.ar>
2024-03-13 15:51:14 -0300

    Subo primer clase del proyecto

---
+++ src/main/java/Cuestionario/Pregunta.java
@@ -0,0 +1,38 @@
+package Cuestionario;
+
+import java.util.List;
+
+public class Pregunta {
+    private String pregunta;
+    private String foto; //Ruta a la foto asociada a la pregunta, puede ser nulo
+    private List<String> opciones;
+    private int respuestaCorrecta; //Indice dentro de la lista de opciones para la respuesta correcta
+
+    public Pregunta(String pregunta, String foto, List<String> opciones, int respuestaCorrecta) {
+        this.pregunta = pregunta;
+        this.foto = foto;
+        this.opciones = opciones;
+        this.respuestaCorrecta = respuestaCorrecta;
+    }
+
+    public Pregunta(String pregunta, List<String> opciones, int respuestaCorrecta) {
+        this(pregunta, null, opciones, respuestaCorrecta);
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public List<String> getOpciones() {
+        return opciones;
+    }
+
+    public int getRespuestaCorrecta() {
+        return respuestaCorrecta;
+    }
+
+}
